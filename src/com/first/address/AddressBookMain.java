6c3a8326febbddd4365445e7f3ba6dad97680ccf
Komal Shinde <komalshinde8299@gmail.com>
2023-05-30 02:58:48 +0530

    Added:Code to create details in address book

--- src/com/first/address/AddressBookMain.java
+++ src/com/first/address/AddressBookMain.java
@@ -3,6 +3,30 @@ package com.first.address;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("* Welcome to Address Book Program ");
+        Contact contact = new Contact();
+        contact.setFirstName("Komal");
+        contact.setLastName("Shinde");
+        contact.setCity("Pune");
+        contact.setPhoneNo("9922359618");
+        contact.setAddress("Pune, Maharastra, India");
+        contact.setState("Maharastra");
+        contact.setEmail("komalshinde@gmail.com");
+        contact.setZipCode(5678);
+
+        System.out.println(contact.getFirstName());
+        System.out.println(contact.getLastName());
+        System.out.println(contact.getAddress());
+        System.out.println(contact.getCity());
+        System.out.println(contact.getState());
+        System.out.println(contact.getPhoneNo());
+        System.out.println(contact.getZipCode());
+        System.out.println(contact.getEmail());
+
+
+
+
+
+
     }
 
 }
