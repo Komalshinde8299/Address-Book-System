ed0a2b625868719ef96a803e3ee01f9928e1ac96
Komal Shinde <komalshinde8299@gmail.com>
2023-06-06 19:27:59 +0530

    Added:Code to add multiple address book is added

--- src/com/first/address/AddressBookMain.java
+++ src/com/first/address/AddressBookMain.java
@@ -6,11 +6,15 @@ public class AddressBookMain{
 
     public static void main(String[] args) {
         System.out.println("* Welcome to Address Book Program ");
-
+        System.out.println("If you want to add contact to Maharastra Zone Address book then enter '1'");
+        System.out.println("If you want to add contact to Kerala Zone Address book then enter '2'");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("For storing multiple contacts in address book");
-        System.out.println("Enter no of contacts you want to add ");
+        int checkZone = scanner.nextInt();
+        if(checkZone == 1){
+            System.out.println("* Welcome to Maharastra Zone Address Book ");
+
+            System.out.println("\nEnter no of contacts you want to add ");
 
         int no = scanner.nextInt();
         AddressBook addressBook = new AddressBook(no);
@@ -60,6 +64,12 @@ public class AddressBookMain{
 
         System.out.println("Contacts from address book");
         addressBook.display();
+        } else if (checkZone == 2){
+
+            mangeAddressBook();
+        } else {
+            System.out.println(" Invalid input");
+        }
 
 //        System.out.println("\nEnter First Name of contact which you want to remove");
 //        String name = scanner.nextLine();
@@ -67,6 +77,62 @@ public class AddressBookMain{
 //        addressBook.removeContact(name);
 //        addressBook.display();
 
+
+    }
+    public static void mangeAddressBook(){
+        System.out.println("* Welcome to Kerala Zone Address Book ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nEnter no of contacts you want to add ");
+
+        int no = scanner.nextInt();
+        AddressBook addressBook = new AddressBook(no);
+        scanner.nextLine();
+
+        for (int i = 0; i < no; i++) {
+            Contact obj = new Contact();
+
+
+            System.out.println("Enter your first name");
+            String firstName = scanner.nextLine();
+
+            obj.setFirstName(firstName);
+
+            System.out.println("Enter your last name");
+            String lastName = scanner.nextLine();
+            obj.setLastName(lastName);
+
+            System.out.println("Enter your full address");
+            String address = scanner.nextLine();
+            obj.setAddress(address);
+
+            System.out.println("Enter your City");
+            String city = scanner.nextLine();
+            obj.setCity(city);
+
+            System.out.println("Enter your State");
+            String state = scanner.nextLine();
+            obj.setState(state);
+
+            System.out.println("Enter your Zip");
+            int zipCode = scanner.nextInt();
+            obj.setZipCode(zipCode);
+            scanner.nextLine();
+
+            System.out.println("Enter your Phone No");
+            String phoneNo = scanner.nextLine();
+            obj.setPhoneNo(phoneNo);
+
+            System.out.println("Enter your Email");
+            String email = scanner.nextLine();
+            obj.setPhoneNo(email);
+
+            addressBook.addcontacts(obj);
+
+        }
+
+        System.out.println("Contacts from address book");
+        addressBook.display();
 
     }
 
